91c5f760fb96e1367ccbb9ac6f85f94710df8bc0
andreygp5 <andreygp555@gmail.com>
2021-08-15 17:37:53 +0300

    feat: add auth and login guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './core/pages/not-found/not-found.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { LoginGuard } from './core/guards/login.guard';
 
 const routes: Routes = [
   {
     path: 'youtube',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./youtube/youtube.module').then(m => m.YoutubeModule),
   },
   {
     path: 'auth',
+    canActivate: [LoginGuard],
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
   },
   {
---
+++ src/app/core/guards/login.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanActivateChild, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../auth/services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanActivate, CanActivateChild {
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    const isLoggedIn = this.authService.isLoggedIn();
+    if (!isLoggedIn) {
+      return true;
+    }
+
+    this.router.navigate(['/youtube']).then();
+    return false;
+  }
+
+  canActivateChild(): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate();
+  }
+}
