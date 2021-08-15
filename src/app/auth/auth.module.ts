98b38a42f7f69813fe0c6aedb449690d6d612eb8
andreygp5 <andreygp555@gmail.com>
2021-08-16 00:03:54 +0300

    refactor: remove modules from individual pages

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginModule } from './pages/login/login.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+  ],
   imports: [
     CommonModule,
+    SharedModule,
     AuthRoutingModule,
-    LoginModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    LoginComponent,
   ],
 })
 export class AuthModule {
