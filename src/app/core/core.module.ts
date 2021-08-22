87cf483e091cc49dfa2df6257969659bd00e93fc
andreygp5 <andreygp555@gmail.com>
2021-08-22 12:22:53 +0300

    refactor: change folder name

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderModule } from './modules/header/header.module';
+import { HeaderModule } from './components/header/header.module';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { SharedModule } from '../shared/shared.module';
 
